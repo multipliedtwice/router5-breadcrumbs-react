e34a05a7cc1b38ada3ea5dd8bc36c3d3061f23b0
Danil <thousandsofraccoons@gmail.com>
2020-02-28 19:53:29 +0700

    Iframes bring back

--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -1,5 +1,5 @@
 import React, { Suspense } from 'react'
-import { Hero, Features, Tabss, Comments } from '../../components'
+import { Hero, Features, Iframes, Comments } from '../../components'
 import { hero, features } from './data'
 import { Header } from '../../components/header'
 import { Footer } from '../../components/footer'
@@ -16,7 +16,7 @@ export const Homepage = () => {
           <Features features={features} />
         </Suspense>
         <hr />
-        <Tabss />
+        <Iframes />
         <Comments />
       </div>
       <Footer />
